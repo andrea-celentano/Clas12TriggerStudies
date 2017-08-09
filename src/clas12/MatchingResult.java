f40bffada43713e2ec1b53ad6596b4380fbfe6da
Andrea Celentano <andrea.celentano@ge.infn.it>
2017-08-09 19:55:17 +0200

    Working on MC/recon matching

---
+++ src/clas12/MatchingResult.java
@@ -0,0 +1,23 @@
+package clas12;
+
+public class MatchingResult {
+    int result;
+    double minDistance;
+
+    public double getMinDistance() {
+        return minDistance;
+    }
+
+    public void setMinDistance(double minDistance) {
+        this.minDistance = minDistance;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int status) {
+        this.result = status;
+    }
+
+}
